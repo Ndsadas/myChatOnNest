112ea285e8ca3586db2398bae8d270ec2c1047de
Samir Bagiev <bagsamitoo@gmail.com>
2024-02-12 14:47:40 +0300

    add

---
+++ src/news/news.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NewsController } from './news.controller';
+import { NewsService } from './news.service';
+import { CommentsModule } from './comments/comments.module';
+import { MailModule } from '../mail/mail.module';
+import { NewsEntity } from './news.entity';
+import { UsersModule } from '../users/users.module';
+import { RolesGuard } from '../auth/role/roles.guard';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  controllers: [NewsController],
+  providers: [NewsService],
+  imports: [
+    TypeOrmModule.forFeature([NewsEntity]),
+    CommentsModule,
+    MailModule,
+    UsersModule,
+  ],
+  exports: [TypeOrmModule.forFeature([NewsEntity]), NewsService],
+})
+export class NewsModule {}
